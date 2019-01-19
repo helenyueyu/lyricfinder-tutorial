bf1b5020375f6e6796a9ba2df4423a44f5440450
tokyosuite <helenyueyu@gmail.com>
2019-01-18 18:03:18 -0800

    Updates

---
+++ precache-manifest.68e8132c581cbbb706b90d20a74a7b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b473ea99bed215f271e8",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/static/css/main.03ce951c.chunk.css"
+  },
+  {
+    "revision": "b473ea99bed215f271e8",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/static/js/main.b473ea99.chunk.js"
+  },
+  {
+    "revision": "776401a9fdb6b262e7f2",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/static/js/1.776401a9.chunk.js"
+  },
+  {
+    "revision": "df9bd9b43683d0c71261",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/static/js/runtime~main.df9bd9b4.js"
+  },
+  {
+    "revision": "4cc848b538580780aa52b50a7a18e1fa",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/static/media/spinner.4cc848b5.gif"
+  },
+  {
+    "revision": "cbad480614a9d7249d0f0947f75ea5e9",
+    "url": "tokyosuite.github.io/lyricfinder-tutorial/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "tokyosuite.github.io/lyricfinder-tutorial/precache-manifest.68e8132c581cbbb706b90d20a74a7b43.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("tokyosuite.github.io/lyricfinder-tutorial/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
